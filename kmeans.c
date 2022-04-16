828033232c7c6072f1f8021636e580e77ea14434
stavbentov <79993977+stavbentov@users.noreply.github.com>
2022-04-16 15:14:54 +0300

    for decomendtion

--- kmeans.c
+++ kmeans.c
@@ -5,57 +5,56 @@
 #include <float.h>
 #include <ctype.h>
 
-int check_euclidean_norm(double **newCentroids ,double **oldCentroids,int dimension, int K);
-int find_cluster(double **Centroids,double *Datapoint,int dimension,int K);
-void free_memory(double ** Centroids,double **Datapoints,int K,int N);
+int check_euclidean_norm(double **newCentroids, double **oldCentroids, int dimension, int K);
+int find_cluster(double **Centroids, double *Datapoint, int dimension, int K);
+void free_memory(double **Centroids, double **Datapoints, int K, int N);
 int kMeans(int K, int max_iter, const char *input_filename, const char *output_filename);
-void updateOldCentroid(double **newCentroids ,double **oldCentroids,int dimension, int K);
+void updateOldCentroid(double **newCentroids, double **oldCentroids, int dimension, int K);
 int is_number(const char *argument);
 int is_txt(const char *argument);
 
-static const double EPSILON=0.001;
+static const double EPSILON = 0.001;
 
 int main(int argc, char const *argv[])
 {
 
-    int i,isValid,K,max_iter;
-    max_iter=200;
-    isValid=1;
+    int i, isValid, K, max_iter;
+    max_iter = 200;
+    isValid = 1;
 
     /* invalid number of arguments*/
-    if(argc<4 || argc>5)
+    if (argc < 4 || argc > 5)
     {
         printf("Invalid Input!");
         return 1;
     }
 
-
-    for(i=1;i<argc;i++)
+    for (i = 1; i < argc; i++)
     {
-        if(argc==5)
+        if (argc == 5)
         {
-            if(i==1 || i==2)
+            if (i == 1 || i == 2)
             {
-                isValid=is_number(argv[i]);
+                isValid = is_number(argv[i]);
             }
-            if(i==3 || i==4)
+            if (i == 3 || i == 4)
             {
-                isValid=is_txt(argv[i]);
+                isValid = is_txt(argv[i]);
             }
         }
         else
         {
-            if(i==1)
+            if (i == 1)
             {
-                isValid=is_number(argv[i]);
+                isValid = is_number(argv[i]);
             }
-            if(i==2 || i==3)
+            if (i == 2 || i == 3)
             {
-                isValid=is_txt(argv[i]);
+                isValid = is_txt(argv[i]);
             }
         }
 
-        if(!isValid)
+        if (!isValid)
         {
             printf("Invalid Input!");
             return 1;
@@ -63,65 +62,64 @@ int main(int argc, char const *argv[])
     }
 
     K = atoi(argv[1]);
-    if(argc==5)
+    if (argc == 5)
     {
-        max_iter=atoi(argv[2]);
-        return kMeans(K,max_iter,argv[3],argv[4]);
+        max_iter = atoi(argv[2]);
+        return kMeans(K, max_iter, argv[3], argv[4]);
     }
 
-    return kMeans(K,max_iter,argv[2],argv[3]);
+    return kMeans(K, max_iter, argv[2], argv[3]);
 }
 
-
-int check_euclidean_norm(double **newCentroids ,double **oldCentroids,int dimension, int K)
+int check_euclidean_norm(double **newCentroids, double **oldCentroids, int dimension, int K)
 {
-    int i,j;
+    int i, j;
     double sum;
-    for(i=0;i<K;i++)
+    for (i = 0; i < K; i++)
     {
-        sum=0;
-        for(j=0;j<dimension;j++)
+        sum = 0;
+        for (j = 0; j < dimension; j++)
         {
-            sum+=pow(newCentroids[i][j]-oldCentroids[i][j],2);
+            sum += pow(newCentroids[i][j] - oldCentroids[i][j], 2);
         }
-        if(sqrt(sum)>=EPSILON)
+        if (sqrt(sum) >= EPSILON)
             return 0;
     }
     /* every centroids changed doesn't changed more then epsilon */
     return 1;
 }
 
-int find_cluster(double **Centroids,double *Datapoint,int dimension,int K)
+int find_cluster(double **Centroids, double *Datapoint, int dimension, int K)
 {
-    int i,j,cluster;
-    double sum,minSum;
+    int i, j, cluster;
+    double sum, minSum;
 
-    minSum=DBL_MAX;
-    for(i=0;i<K;i++)
+    minSum = DBL_MAX;
+    for (i = 0; i < K; i++)
     {
-        sum=0;
-        for(j=0;j<dimension;j++)
+        sum = 0;
+        for (j = 0; j < dimension; j++)
         {
-            sum+=pow((Datapoint[j]-Centroids[i][j]),2);
+            sum += pow((Datapoint[j] - Centroids[i][j]), 2);
         }
-        if(minSum>=sum)
+        if (minSum >= sum)
         {
-            minSum=sum;
+            minSum = sum;
 
             /* cluster number i+1 because in cell number i*/
-            cluster=i+1;
+            cluster = i + 1;
         }
     }
 
     return cluster;
 }
 
-void free_memory(double ** Centroids,double **Datapoints,int K,int N)
+void free_memory(double **Centroids, double **Datapoints, int K, int N)
 {
     int i;
-    for(i=0;i<N;i++)
+    for (i = 0; i < N; i++)
     {
-        if(i<K)
+        if (i < K)
         {
             free(Centroids[i]);
         }
@@ -138,36 +136,36 @@ int kMeans(int K, int max_iter, const char *input_filename, const char *output_f
     N= number of vectors
     dimension= vector's dimension
     firstLine= helps know when we are scanning the first line and only then calculate the dimension
-    i, j, counter= counters for loop iteretions 
+    i, j, counter= counters for loop iteretions
     corr= saves each string we scan and helps converting to double
     Datapoints= saves all datapoint's vectors
     Centroids= saves all centroid's vectors (updated)
     oldCentroids= saves all centroid's vectors (before change)
     */
-    FILE *ifp=NULL;
+    FILE *ifp = NULL;
     char c;
-    int N,dimension;
+    int N, dimension;
     int firstLine;
-    int i,j,counter;
+    int i, j, counter;
     double corr;
     double **Datapoints;
     double **Centroids;
     double **oldCentroids;
     int cluster;
 
-    if(max_iter<=0)
+    if (max_iter <= 0)
     {
         printf("Invalid Input!");
         return 1;
     }
 
-    i=0;
-    j=0;
-    dimension=1;
-    N=0;
-    firstLine=1;
-    counter=0;
-    ifp= fopen(input_filename,"r");
+    i = 0;
+    j = 0;
+    dimension = 1;
+    N = 0;
+    firstLine = 1;
+    counter = 0;
+    ifp = fopen(input_filename, "r");
 
     if (ifp == NULL)
     {
@@ -175,163 +173,164 @@ int kMeans(int K, int max_iter, const char *input_filename, const char *output_f
         return 1;
     }
 
-    while((c=fgetc(ifp))!=EOF)
+    while ((c = fgetc(ifp)) != EOF)
     {
-        if(firstLine)
-            if(c==',')
+        if (firstLine)
+            if (c == ',')
                 dimension++;
-        if(c=='\n')
+        if (c == '\n')
         {
             N++;
-            if(firstLine)
+            if (firstLine)
             {
-                firstLine=0;
+                firstLine = 0;
             }
         }
     }
 
-
-    if(K>N || K <= 0)
+    if (K > N || K <= 0)
     {
         printf("Invalid Input!");
         return 1;
     }
 
-
     rewind(ifp);
-    Datapoints=malloc((sizeof(double*))*N);
-    Centroids=malloc((sizeof(double*))*K);
-    oldCentroids=malloc((sizeof(double*))*K);
+    Datapoints = malloc((sizeof(double *)) * N);
+    Centroids = malloc((sizeof(double *)) * K);
+    oldCentroids = malloc((sizeof(double *)) * K);
 
-    if(Datapoints == NULL || Centroids == NULL){
+    if (Datapoints == NULL || Centroids == NULL)
+    {
         printf("An Error Has Occurred");
         return 1;
     }
 
-    for(i=0;i<N;i++)
+    for (i = 0; i < N; i++)
     {
         /* last cell contains the datapoint's cluster*/
-        Datapoints[i]=malloc((sizeof(double))*(dimension+1));
-        if(Datapoints[i] == NULL){
+        Datapoints[i] = malloc((sizeof(double)) * (dimension + 1));
+        if (Datapoints[i] == NULL)
+        {
             printf("An Error Has Occurred");
             return 1;
         }
-        if(i<K)
+        if (i < K)
         {
             /* last call contains number of datapoints assigned to centroid's cluster*/
-            Centroids[i]=malloc((sizeof(double))*(dimension+1));
-            oldCentroids[i]=malloc((sizeof(double))*(dimension));
-            if(Centroids[i] == NULL){
+            Centroids[i] = malloc((sizeof(double)) * (dimension + 1));
+            oldCentroids[i] = malloc((sizeof(double)) * (dimension));
+            if (Centroids[i] == NULL)
+            {
                 printf("An Error Has Occurred");
                 return 1;
             }
         }
 
-        for(j=0;j<dimension;j++)
+        for (j = 0; j < dimension; j++)
         {
-            if(fscanf(ifp,"%lf",&corr)==1)
+            if (fscanf(ifp, "%lf", &corr) == 1)
             {
-                Datapoints[i][j]=(double)corr;
-                if(i<K)
+                Datapoints[i][j] = (double)corr;
+                if (i < K)
                 {
-                    Centroids[i][j]=(double)corr;
-                    oldCentroids[i][j]=(double)corr;
+                    Centroids[i][j] = (double)corr;
+                    oldCentroids[i][j] = (double)corr;
                 }
                 fgetc(ifp);
             }
         }
 
-        Datapoints[i][j]=0;
-        if(i<K)
+        Datapoints[i][j] = 0;
+        if (i < K)
         {
-            Centroids[i][j]=0;
+            Centroids[i][j] = 0;
             /*Centroids[i][j+1]=0;*/
         }
     }
 
     fclose(ifp);
 
-    while(max_iter>counter)
+    while (max_iter > counter)
     {
-        for(i=0;i<N;i++)
+        for (i = 0; i < N; i++)
         {
-            cluster=find_cluster(Centroids,Datapoints[i],dimension,K);
-            Datapoints[i][dimension]=cluster;
-            Centroids[cluster-1][dimension]+=1;
+            cluster = find_cluster(Centroids, Datapoints[i], dimension, K);
+            Datapoints[i][dimension] = cluster;
+            Centroids[cluster - 1][dimension] += 1;
         }
 
-        for(i=0;i<K;i++)
+        for (i = 0; i < K; i++)
         {
-            for(j=0;j<dimension;j++)
+            for (j = 0; j < dimension; j++)
             {
-                Centroids[i][j]=0;
+                Centroids[i][j] = 0;
             }
         }
 
-        for(i=0;i<N;i++)
+        for (i = 0; i < N; i++)
         {
-            cluster=Datapoints[i][dimension];
-            for(j=0;j<dimension;j++)
+            cluster = Datapoints[i][dimension];
+            for (j = 0; j < dimension; j++)
             {
-                Centroids[cluster-1][j]+=Datapoints[i][j];
+                Centroids[cluster - 1][j] += Datapoints[i][j];
             }
         }
 
-        for(i=0;i<K;i++)
+        for (i = 0; i < K; i++)
         {
-            for(j=0;j<dimension;j++)
+            for (j = 0; j < dimension; j++)
             {
-                Centroids[i][j]=Centroids[i][j]/Centroids[i][dimension];
+                Centroids[i][j] = Centroids[i][j] / Centroids[i][dimension];
             }
-            Centroids[i][dimension]=0;
+            Centroids[i][dimension] = 0;
         }
 
-        if(check_euclidean_norm(Centroids,oldCentroids,dimension,K))
+        if (check_euclidean_norm(Centroids, oldCentroids, dimension, K))
         {
             break;
         }
-        
-        updateOldCentroid(Centroids,oldCentroids,dimension,K);
+
+        updateOldCentroid(Centroids, oldCentroids, dimension, K);
 
         counter++;
     }
 
-    ifp= fopen(output_filename,"w");
+    ifp = fopen(output_filename, "w");
     if (ifp == NULL)
     {
         printf("An Error Has Occurred");
         return 1;
     }
 
-    for(i=0;i<K;i++)
+    for (i = 0; i < K; i++)
     {
-        for(j=0;j<dimension;j++)
+        for (j = 0; j < dimension; j++)
         {
-            if(j==dimension-1)
+            if (j == dimension - 1)
             {
                 fprintf(ifp, "%.4f", Centroids[i][j]);
             }
             else
                 fprintf(ifp, "%.4f,", Centroids[i][j]);
         }
-        fprintf(ifp,"\n");
+        fprintf(ifp, "\n");
     }
 
     fclose(ifp);
 
-    free_memory(Centroids,Datapoints,K,N);
+    free_memory(Centroids, Datapoints, K, N);
 
     return 0;
 }
 
-void updateOldCentroid(double **newCentroids ,double **oldCentroids,int dimension, int K)
+void updateOldCentroid(double **newCentroids, double **oldCentroids, int dimension, int K)
 {
-    int i,j;
-    for(i=0;i<K;i++)
+    int i, j;
+    for (i = 0; i < K; i++)
     {
-        for(j=0;j<dimension;j++)
+        for (j = 0; j < dimension; j++)
         {
-            oldCentroids[i][j]=newCentroids[i][j];
+            oldCentroids[i][j] = newCentroids[i][j];
         }
     }
 }
@@ -340,50 +339,49 @@ int is_number(const char *argument)
 {
     int i;
     char c;
-    i=0;
-    c=argument[0];
-    while (c!='\0')
+    i = 0;
+    c = argument[0];
+    while (c != '\0')
     {
         i++;
-        if(!isdigit(c))
+        if (!isdigit(c))
             return 0;
-        c=argument[i];
+        c = argument[i];
     }
     return 1;
 }
 
 int is_txt(const char *argument)
 {
-    int i,j;
+    int i, j;
     char c;
     char strArg[4];
 
     char str[] = ".txt";
-    i=0;
-    j=0;
-    c=argument[0];
+    i = 0;
+    j = 0;
+    c = argument[0];
 
-    while (c!='\0')
+    while (c != '\0')
     {
         i++;
-        c=argument[i];
+        c = argument[i];
     }
-    if(i<5)
+    if (i < 5)
     {
         return 0;
     }
 
-    i=i-4;
+    i = i - 4;
 
-    while(j<=4)
+    while (j <= 4)
     {
-        c=argument[i+j];
-        strArg[j]=c;
+        c = argument[i + j];
+        strArg[j] = c;
         j++;
     }
 
-
-    if(strcmp(strArg,str))
+    if (strcmp(strArg, str))
     {
         return 0;
     }
--- kmeans.py
+++ kmeans.py
@@ -3,16 +3,18 @@ import sys
 
 from pip import main
 
-EPSILON=0.001
+EPSILON = 0.001
 
-def kMeans(K, max_iter,input_filename, output_filename):
+
+def kMeans(K, max_iter, input_filename, output_filename):
 
     fileOpener = open(input_filename, "r")
-    
-    data_points_array = [] # saves the datapoint x1, ... , xN (array DxN)
-    Centroids_array = [] # saves the centroids u1, ... , uK
-    oldCentroids_array = [] # saves the unupdated centroids u1, ... , uK
-    Centroids_size_array = [0 for i in range(K)] # saves clusters size |S1|, ..., |SK|
+
+    data_points_array = []  # saves the datapoint x1, ... , xN (array DxN)
+    Centroids_array = []  # saves the centroids u1, ... , uK
+    oldCentroids_array = []  # saves the unupdated centroids u1, ... , uK
+    # saves clusters size |S1|, ..., |SK|
+    Centroids_size_array = [0 for i in range(K)]
     counter = 0
 
     # build data_points_array
@@ -21,20 +23,20 @@ def kMeans(K, max_iter,input_filename, output_filename):
         if line == '':
             break
         data_points_array.append([float(x) for x in line.split(",")])
-        if counter < K :
+        if counter < K:
             Centroids_array.append([float(x) for x in line.split(",")])
             oldCentroids_array.append([float(x) for x in line.split(",")])
         counter += 1
     fileOpener.close()
-    ### end of while loop
+    # end of while loop
 
-    N=len(data_points_array)
+    N = len(data_points_array)
 
-    if(K>N or K <= 0):
+    if(K > N or K <= 0):
         print("Invalid Input!")
         sys.exit()
 
-    dimension=len(data_points_array[0])
+    dimension = len(data_points_array[0])
 
     # for each 0<=i<N data_points_cluster[i]=S for s is the cluster of xi
     data_points_cluster = [0 for i in range(N)]
@@ -45,28 +47,33 @@ def kMeans(K, max_iter,input_filename, output_filename):
         # calculate cluster for each datapoint
         for i in range(N):
             # update each datapoints's cluster
-            data_points_cluster[i] = find_cluster(Centroids_array, data_points_array[i]) # update cluster array
-            # updtae number of datapoints that belongs to each cluster 
-            Centroids_size_array[data_points_cluster[i] - 1] += 1 # update cluster's size
+            data_points_cluster[i] = find_cluster(
+                Centroids_array, data_points_array[i])  # update cluster array
+            # updtae number of datapoints that belongs to each cluster
+            # update cluster's size
+            Centroids_size_array[data_points_cluster[i] - 1] += 1
 
-        Centroids_array = [[float(0) for i in range (dimension)] for j in range(K)]
-        
+        Centroids_array = [[float(0) for i in range(dimension)]
+                           for j in range(K)]
+
         # calculate updated clusters
         for i in range(len(data_points_array)):
-            add_vectors(Centroids_array[data_points_cluster[i]-1], data_points_array[i],dimension)
+            add_vectors(
+                Centroids_array[data_points_cluster[i]-1], data_points_array[i], dimension)
         for i in range(len(Centroids_array)):
-            div_vectors(Centroids_array[i], Centroids_size_array[i],dimension)
-        
-        Centroids_size_array=[0 for i in range(K)]
+            div_vectors(Centroids_array[i], Centroids_size_array[i], dimension)
 
-        if(check_euclidean_norm(Centroids_array,oldCentroids_array,dimension,K)):
+        Centroids_size_array = [0 for i in range(K)]
+
+        if(check_euclidean_norm(Centroids_array, oldCentroids_array, dimension, K)):
             break
-        
-        updateOldCentroid(Centroids_array,oldCentroids_array,dimension,K)
 
-        counter+=1
+        updateOldCentroid(Centroids_array, oldCentroids_array, dimension, K)
 
-    Centroids_array = [['%.4f' % (Centroids_array[j][i]) for i in range (len(data_points_array[0]))] for j in range(K)]
+        counter += 1
+
+    Centroids_array = [['%.4f' % (Centroids_array[j][i]) for i in range(
+        len(data_points_array[0]))] for j in range(K)]
     fileOpener = open(output_filename, "w")
 
     for mean in Centroids_array:
@@ -75,31 +82,33 @@ def kMeans(K, max_iter,input_filename, output_filename):
                 fileOpener.write(str(mean[i]) + ",")
             else:
                 fileOpener.write(str(mean[i]) + "\n")
-    
-    fileOpener.close()
-    
 
-def check_euclidean_norm(Centroids_array,oldCentroids_array,dimension,K):
+    fileOpener.close()
+
+
+def check_euclidean_norm(Centroids_array, oldCentroids_array, dimension, K):
     for i in range(K):
         sum = 0
         for j in range(dimension):
             sum += ((Centroids_array[i][j]-oldCentroids_array[i][j])**2)
-        # if there exist a centroid that changes more then epsilon- return false 
+        # if there exist a centroid that changes more then epsilon- return false
         if math.sqrt(sum) >= EPSILON:
             return False
     # every centroid hasn't changed more then epsilon
     return True
 
 # find the corresponding cluster of datapoint
+
+
 def find_cluster(Centroids_array, dataPoint):
     meanIndex = 0
     minSum = math.inf
     index = 0
 
-    for mean in Centroids_array :
+    for mean in Centroids_array:
         index += 1
         sum = 0
-        
+
         for i in range(len(dataPoint)):
             sum += (dataPoint[i] - mean[i])**2
 
@@ -108,52 +117,57 @@ def find_cluster(Centroids_array, dataPoint):
             meanIndex = index
     return meanIndex
 
-def add_vectors(mean, data_point,dimension):
+
+def add_vectors(mean, data_point, dimension):
     for i in range(dimension):
         mean[i] += data_point[i]
 
-def div_vectors(mean, clusterSize,dimension):
+
+def div_vectors(mean, clusterSize, dimension):
     if clusterSize != 0:
         for i in range(dimension):
             mean[i] = mean[i] / clusterSize
 
-def updateOldCentroid(newCentroids ,oldCentroids,dimension,K):
+
+def updateOldCentroid(newCentroids, oldCentroids, dimension, K):
     for i in range(K):
         for j in range(dimension):
-            oldCentroids[i][j]=newCentroids[i][j]
+            oldCentroids[i][j] = newCentroids[i][j]
+
 
 def main(argv):
-    max_iter=200
-    isValid=True
-    argLen=len(argv)
-    if(argLen<4 or argLen>5):
-        print("Inavlid Input!")
-        sys.exit()
-    
-    for i in range(argLen):
-        if(argLen==5):
-            if(i==1 or i==2):
-                isValid=argv[i].isnumeric()
-            if(i==3 or i==4):
-                isValid=(argv[i][-4:]==".txt")
-        else:
-            if(i==1):
-                isValid=argv[i].isnumeric()
-            if(i==2 or i==3):
-                isValid=(argv[i][-4:]==".txt")
-        if(isValid==False):
-            print("Inavlid Input!")
-            sys.exit()
-    
-    if(max_iter<=0):
+    max_iter = 200
+    isValid = True
+    argLen = len(argv)
+    if(argLen < 4 or argLen > 5):
         print("Inavlid Input!")
         sys.exit()
 
-    K=(int)(argv[1])
-    if(argLen==5):
-        max_iter=(int)(argv[2])
-        return kMeans(K,max_iter,argv[3],argv[4])
-    return kMeans(K,max_iter,argv[2],argv[3])
+    for i in range(argLen):
+        if(argLen == 5):
+            if(i == 1 or i == 2):
+                isValid = argv[i].isnumeric()
+            if(i == 3 or i == 4):
+                isValid = (argv[i][-4:] == ".txt")
+        else:
+            if(i == 1):
+                isValid = argv[i].isnumeric()
+            if(i == 2 or i == 3):
+                isValid = (argv[i][-4:] == ".txt")
+        if(isValid == False):
+            print("Inavlid Input!")
+            sys.exit()
+
+    if(max_iter <= 0):
+        print("Inavlid Input!")
+        sys.exit()
+
+    K = (int)(argv[1])
+    if(argLen == 5):
+        max_iter = (int)(argv[2])
+        return kMeans(K, max_iter, argv[3], argv[4])
+    return kMeans(K, max_iter, argv[2], argv[3])
+
 
 if __name__ == "__main__":
     main(sys.argv)
